edbbd8effa43e74ae4729a0565999c5176b6d1c1
x-cold <lxstart@outlook.com>
2021-12-04 19:04:17 +0800

    chore: add v3 test

--- test/v3/v3.test.ts
+++ test/v3/v3.test.ts
@@ -1,13 +1,9 @@
-import { Version3Client } from '../src';
+import { Version3Client } from '../../src';
 
 /**
  * Dummy test
  */
-describe('Dummy test', () => {
-  it('Version3Client is instantiable', () => {
-    expect(Version3Client).toBeInstanceOf(Object);
-  });
-
+describe('V3 test', () => {
   it('Version3Client instance', () => {
     const client = new Version3Client({
       baseURL: 'https://your-domain.atlassian.net',
